8c4a60d7404992239825fc85e39965780be3559c
jwilliamson1 <joseph.williamson15@gmail.com>
2019-05-08 07:53:08 -0400

    remove folr AsInt

--- ch04/AsInt.hs
+++ ch04/AsInt.hs
@@ -2,7 +2,4 @@ import Data.Char (digitToInt)
 
 asInt :: String -> Int
 asInt xs = foldl convert 0 xs
-                where convert acc x = acc * 10 + digitToInt x
-
-convert :: (Int, Int) -> Char -> (Int, Int) 
-convert (value,factor) c = (factor * digitToInt c + value, factor * 10)
+              where convert acc x = acc * 10 + digitToInt x
