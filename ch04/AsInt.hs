2a4c0046d91e949cf3945db782d15134ba138be5
Joseph Williamson <jwilliamson@kbra.com>
2019-05-10 17:37:59 -0400

    add asInt_Either

--- ch04/AsInt.hs
+++ ch04/AsInt.hs
@@ -1,6 +1,18 @@
-import Data.Char (digitToInt)
+import Data.Char (digitToInt, isDigit)
+import Data.List(foldl')
 
 asInt :: String -> Int
-asInt xs = let numericPart = foldl convert 0 xs
+asInt [] = 0
+asInt (x:xs) = if x == '-' then negate (numericPart xs) else numericPart (x:xs)
+    where numericPart [] =  0
+          numericPart (x:xs) = foldl' convert 0 (x:xs)
                                 where convert acc x = acc * 10 + digitToInt x
-          in numericPart
+               
+asInt_Either :: String -> Either String Int
+asInt_Either [] = Left "empty string"
+asInt_Either ('-':[]) = Left "negative (-) with no number"
+asInt_Either ('-':xs) = Left "negative number"
+asInt_Either (x:xs) = numericPart (x:xs)
+    where numericPart (x:xs) = foldl' convert (Right 0) (x:xs)
+                                where convert (Right acc) x = if isDigit(x) then Right (acc * 10 + digitToInt x) else Left ("'" ++ [x] ++ "' is not a digit")
+                                      convert (Left e) _ = (Left e)
